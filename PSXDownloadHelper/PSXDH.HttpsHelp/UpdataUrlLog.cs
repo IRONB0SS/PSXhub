1b1c446b3a2c4d090415a35e51cd621d4376ef22
IRONB0SS <97769328+IRONB0SS@users.noreply.github.com>
2025-02-28 03:23:53 -0800

    Delete PSXDownloadHelper directory

--- PSXDownloadHelper/PSXDH.HttpsHelp/UpdataUrlLog.cs
+++
@@ -1,6 +0,0 @@
-﻿using PSXDH.Model;
-
-namespace PSXDH.HttpsHelp
-{
-    public delegate void UpdataUrlLog(UrlInfo urlinfo);
-}
--- PSXDownloadHelper/PSXDH.ProxyHelp/DestroyDelegate.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace PSXDH.ProxyHelp
-{
-    public delegate void DestroyDelegate(Client client);
-}
